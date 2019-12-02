2cbd2f1dcdbc136e169fab6b2f0750bc035cc4d9
Barak Saidoff <barak.saidoff@gmail.com>
2019-12-02 11:21:22 -0500

    created listings routes and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/listings', to: 'listings#index', as: 'listings'
+  get '/listings/new', to: 'listings#new', as: 'new_listing'
+  get '/listings/:id', to: 'listings#show', as: 'listing'
+  post '/listings', to: 'listings#create'
+  get '/listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
+  patch '/listings/:id', to: 'listings#update'
+  delete '/listings/:id', to: 'listings#delete' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
